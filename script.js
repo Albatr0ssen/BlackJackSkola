31a530f7634b7fecf6d941ff8b2a6d226a24a2c1
Axel Berg <axel.berg@jotunheim.se>
2021-02-07 01:25:04 +0100

    Hit button + One page webiste

--- script.js
+++ script.js
@@ -5,12 +5,35 @@
 let cardColors = ["♠", "♥", "♦", "♣"];
 let cardNumbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Kn", "Q", "K", "E",];
 let cardScore = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
-let dealerSide = document.querySelector("#dealer-side");
-let playerSide = document.querySelector("#player-side");
 let dealerNumber = [rng(), rng()];
 let playerNumber = [rng(), rng()];
 let dealerScore = [cardScore[dealerNumber[0]], cardScore[dealerNumber[1]]];
 let playerScore = [cardScore[playerNumber[0]], cardScore[playerNumber[1]]];
+let main;
+let footer;
+let dealerSide;
+let playerSide;
+
+function gameStart(){
+  body = document.querySelector("body");
+  body.innerHTML = '<header></header>';
+  body.innerHTML += '<main></main>';
+  body.innerHTML += '<footer class="flex-center"></footer>';
+  main = document.querySelector("main");
+  footer = document.querySelector("footer");
+  main.innerHTML = '<div id="dealer-side" class="flex-center"></div>'
+  main.innerHTML += '<div id="line"></div>'
+  main.innerHTML += '<div id="player-side" class="flex-center"></div>'
+  dealerSide = document.querySelector("#dealer-side");
+  playerSide = document.querySelector("#player-side");
+  dealerSide.innerHTML += createCard(createNumber(dealerNumber[0]));
+  dealerSide.innerHTML += '<div class="card hidden"></div>';
+  playerSide.innerHTML += createCard(createNumber(playerNumber[0]));
+  playerSide.innerHTML += createCard(createNumber(playerNumber[1]));
+  footer.innerHTML += '<input id="hit-button" type="button" value="hit" onclick="hit();" />';
+  footer.innerHTML += '<input id="hit-button" type="button" value="stand" onclick="stand();" />';
+  checkWin(playerScore);
+}
 
 function createCard(cardNumber) {
   if (cardNumber == undefined) {
@@ -47,56 +70,51 @@ function totalScore(user) {
   }
   return total;
 }
-
-function checkBlackJack(){
-    if (totalScore("dealerScore") == 21) {
-        if (totalScore("playerScore") == 21) {
-            console.log("push")
-        }
-        else{
-            console.log("dealer blackjack");
-        }
-    }
-    else if (totalScore("playerScore") == 21) {
-        console.log("player blackjack");
-    }
-}
-
 function checkOver21(userScore){
     if(totalScore(userScore) > 21){
         for(let x = 0; x < userScore.length; x++) {
-            console.log(userScore[x]);
             if(userScore[x] == 11){
                 userScore[x] = 1;
-                break;
             }
         }
         
     }
     if(totalScore(userScore) > 21){
         console.log("busted");
+        gameEnd();
     }
     return userScore;
 }
 
-function checkWin(dealerScore, playerScore) {
-    dealerScore = checkOver21(dealerScore);
-    playerScore = checkOver21(playerScore);
-    checkBlackJack();
-    return [dealerScore, playerScore];
+function checkBlackJack(userScore){
+  if(totalScore(userScore) == 21){
+    console.log("blackjack")
+    gameEnd();
+  }
 }
 
-function table(){
-    console.log("Dealer Score: " + totalScore(dealerScore));
-    console.table(dealerScore);
-    console.log("Player Score: "+ totalScore(playerScore));
-    console.table(playerScore);
+function checkWin(userScore) {
+    userScore = checkOver21(userScore);
+    checkBlackJack(userScore);
+    console.table(userScore);
+    console.log(totalScore(playerScore));
 }
 
-dealerSide.innerHTML += createCard(createNumber(dealerNumber[0]));
-dealerSide.innerHTML += '<div class="card hidden"></div>';
-playerSide.innerHTML += createCard(createNumber(playerNumber[0]));
-playerSide.innerHTML += createCard(createNumber(playerNumber[1]));
-[dealerScore, playerScore] = checkWin(dealerScore, playerScore);
-table();
+function hit(){
+  playerNumber[playerNumber.length] = rng();
+  playerScore[playerScore.length] = cardScore[playerNumber[playerNumber.length-1]]
+  playerSide.innerHTML += createCard(createNumber(playerNumber[playerNumber.length-1]));
+  checkWin(playerScore);
+}
 
+function gameMenu(){
+  
+}
+
+
+
+function gameEnd(){
+  footer.innerHTML = '<input id="deal-button" type="button" value="deal" onclick="deal();" />';
+}
+
+gameStart();
